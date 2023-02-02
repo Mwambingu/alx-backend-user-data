4178f1dac324adb45a1de0ffffad8ca8b6ea2b8f
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-02-02 20:40:24 +0300

    Task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""A module for filtering logs.
+"""
+import re
+import logging
+from typing import List
+
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """Filters a log line.
+    """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+if __name__ == "__main__":
+    main()
