24aaf7a5ec3816dc5790e4cfacfc773cb91cac12
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-02-07 22:18:56 +0300

    Hot Fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,13 +13,14 @@ from models.user import User
 class BasicAuth(Auth):
     """Basic authentication class.
     """
+
     def extract_base64_authorization_header(
             self,
             authorization_header: str) -> str:
         """Extracts the Base64 part of the Authorization header
         for a Basic Authentication.
         """
-        if type(authorization_header) == str:
+        if isinstance(authorization_header, str):
             pattern = r'Basic (?P<token>.+)'
             field_match = re.fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
@@ -29,10 +30,10 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(
             self,
             base64_authorization_header: str,
-            ) -> str:
+    ) -> str:
         """Decodes a base64-encoded authorization header.
         """
-        if type(base64_authorization_header) == str:
+        if isinstance(base64_authorization_header, str):
             try:
                 res = base64.b64decode(
                     base64_authorization_header,
@@ -45,11 +46,11 @@ class BasicAuth(Auth):
     def extract_user_credentials(
             self,
             decoded_base64_authorization_header: str,
-            ) -> Tuple[str, str]:
+    ) -> Tuple[str, str]:
         """Extracts user credentials from a base64-decoded authorization
         header that uses the Basic authentication flow.
         """
-        if type(decoded_base64_authorization_header) == str:
+        if isinstance(decoded_base64_authorization_header, str):
             pattern = r'(?P<user>[^:]+):(?P<password>.+)'
             field_match = re.fullmatch(
                 pattern,
@@ -67,7 +68,7 @@ class BasicAuth(Auth):
             user_pwd: str) -> TypeVar('User'):
         """Retrieves a user based on the user's authentication credentials.
         """
-        if type(user_email) == str and type(user_pwd) == str:
+        if isinstance(user_email, str) and isinstance(user_pwd, str):
             try:
                 users = User.search({'email': user_email})
             except Exception:
